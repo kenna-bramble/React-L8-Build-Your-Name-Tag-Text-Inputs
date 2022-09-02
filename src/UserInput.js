5815b9fb3a195aa2d12750331ef7b4531182d9f5
McKenna Bramble <77587638+kenna-bramble@users.noreply.github.com>
2022-09-02 12:35:39 -0400

    Added UserInput component and imported it to App.js

---
+++ src/UserInput.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class UserInput extends Component {
+  state = { name: "" };
+  updateName = (e) => {
+    this.setState({ name: e.target.value });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ name: "" });
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          onChange={this.updateName}
+          type="text"
+          placeholder="Add a new name here..."
+          value={this.state.name}
+        />
+        <input type="submit" value="Create Name Tag" />
+      </form>
+    );
+  }
+}
+
+export default UserInput;
